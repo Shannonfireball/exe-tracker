57406a67630ad8d55371afc61241df4e79c8d57c
Shannonfireball <shannonsaviofernandes@gmail.com>
2022-07-01 13:10:11 -0400

    changing code 2

--- index.js
+++ index.js
@@ -22,111 +22,9 @@ app.get('/', (req, res) => {
 });
 
 
-app.route('/api/users')
-   .post(async (request, response) => {
-  const username  = request.body.username;
-  const result = await User.create({username}).exec();
-  response.json({result});
-})
-.get(async (request,response) => {
-  const result = await User.find();
-  response.json(result);
-  
-})
-
-
-app.post('/api/users/:_id/exercises',(request, response) => {
-  const description = request.body.description;
-  const duration = parseInt(request.body.duration);
-  const date = request.body.date? 'Mon Jan 01 1990':"Fri Jul 01 2022";
-  const id = request.params._id;
-
-
-  const exercise = {
-        date,
-        duration,
-        description,
-        
-        
-  }
-
-  
-  User.findByIdAndUpdate(id,{ $push: { log:exercise }, $inc:{count:1 } }, {new:true}, (error,user) => {
-  if(user){
-    const updatedExercise = {
-                            _id:id,
-                             username:user.username,
-                             ...exercise };
-    // console.log(updatedExercise);
-    response.json(updatedExercise)
-  }
-  })
-
-  
-});
-
-app.get('/api/users/:_id/logs',async (request,response)=>{
-  const { from, to, limit} = request.query
-  const id = request.params._id;
-  console.log(from, to, limit );
-
-  
-  const result = await User.find({ _id:id }).limit(limit).exec();
-  response.json(result);
-})
 
 
 
-
-
-
-// app.get('/api/users/:_id/logs?',(request,response)=>{
-
-
-  
-//   User.findById(request.params._id, (error, user) => {
-      
-//       let responseObject = user;
-    
-//       if(request.query.from || request.query.to || request.query.limit){
-//       if(request.query.from || request.query.to){
-//         const fromDate = new Date(0);
-//         const toDate = new Date();
-//         if(request.query.from){
-//           fromDate = new Date(request.query.from);
-//         }
-//         if(request.query.to){
-//           toDate = new Date(request.query.to);
-//         }
-//         fromDate = fromDate.getTime();
-//         toDate = toDate.getTime();
-
-//         responseObject.log = responseObject.log.filter((log) =>{
-//           let logDate = new Date(log.date).getTime();
-
-//           return logDate >= fromDate && logDate <= toDate;
-//         })
-        
-//       }
-//       if(request.query.limit) {  
-//         responseObject.log = responseObject.log.slice(0,request.query.limit);
-//       }
-//   }    
-
-
-//       responseObject['count'] = responseObject.log.length
-//       response.json(
-//         {
-//           "username": responseObject.username,
-//           "count": responseObject.count,
-//           "_id": responseObject._id,
-//           "log": responseObject.log
-// });
-      
-
-    
-//   })
-// })
 
 
 app.all('*',(request,response) => {
@@ -147,4 +45,3 @@ app.all('*',(request,response) => {
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
 })
-// 8:23:05
